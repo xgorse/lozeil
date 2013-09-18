eb47feb5712a5a0d7bdb9c0828acbdba948401dc
Adrien-D <a.delannoyfr@gmail.com>
2013-09-18 10:10:16 +0200

    Mise en place de graph cubism classique

--- medias/js/timeline.js
+++ medias/js/timeline.js
@@ -8,43 +8,47 @@ window.onload = function () {
 		.size(width)
 		.stop();
 
-	var foo = random("")
+	var foo1 = random1("")
 	d3.select("#cubismtimeline").call(function(div) {
-	  div.datum(foo);
+	  div.datum(foo1);
 	  
 	  div.append("div")
       .attr("class", "axis")
       .call(context.axis().orient("top"));
 
 		div.append("div")
-			.attr("class", "horizon")
+			.attr("class", "horizon_positive")
 			.call(context.horizon()
-			.height(120)
-			.colors(["#bdd7e7","#bae4b3"])
-			.extent([-10, 10])
+			.height(50)
+			.colors(["#08519c", "#3182bd", "#6baed6", "#bdd7e7", "#bae4b3", "#74c476", "#31a354", "#006d2c"])
 		);
-
 	});
 
 	// On mousemove, reposition the chart values to match the rule.
 	context.on("focus", function(i) {
-		d3.selectAll(".value").style("right", i == null ? null : context.size() - (i + 90) + "px");
+		if($("#cubismtimeline .value").eq(0).text() != "NaN") {
+			d3.selectAll("#cubismtimeline .horizon_positive .value").style("display", "block");
+			d3.selectAll("#cubismtimeline .horizon_positive .value").style("right", i == null ? null : context.size() - (i + 90) + "px");
+			d3.selectAll("#cubismtimeline .horizon_negative .value").style("display", "none");
+		} else if ($("#cubismtimeline .value").eq(1).text() != "NaN") {
+			d3.selectAll("#cubismtimeline .horizon_negative .value").style("display", "block");
+			d3.selectAll("#cubismtimeline .horizon_negative .value").style("right", i == null ? null : context.size() - (i) + "px");
+			d3.selectAll("#cubismtimeline .horizon_positive .value").style("display", "none");
+		}
+		//d3.selectAll("#cubismtimeline .value").style("right", i == null ? null : context.size() - (i + 90) + "px");
 	});
 
-	function random(name) {
-	  var value = 0,
-		  values = [],
-		  i = 0,
+	function random1(name) {
+	  var positive_values = [],
+		  negative_values = [],
 		  last;
 		  $(".cubism_data li").each(function () {
-			  values.push($(this).text());
+			positive_values.push($(this).text());
 		  })
-		  
 	  return context.metric(function(start, stop, step, callback) {
 		start = +start, stop = +stop;
 		if (isNaN(last)) last = start;
-		console.log(last, stop);
-		callback(null, values = values.slice((start - stop) / step));
+		callback(null, positive_values = positive_values.slice((start - stop) / step));
 	  }, name);
 	}
 }
