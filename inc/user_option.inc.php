e030f6ffb63084729d965438d9b71b8a4221be68
Perrick Penet-Avez <perrick@noparking.net>
2013-12-10 11:25:26 +0100

    Simplification du User_Option::load()

--- inc/user_option.inc.php
+++ inc/user_option.inc.php
@@ -79,13 +79,6 @@ class User_Option extends Record {
 	}
 
 	function load(array $key = array(), $table = "useroptions", $columns = null) {
-		if (empty($key) or $key['id'] == 0) {
-			if ($this->id === 0) {
-				return false;
-			} else {
-				$key = array ("id" => $this->id);
-			}
-		}
 		return parent::load($key, $table, $columns);
 	}
 }
